59d12b8e08730c85dc027ce4404d3510112ec678
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-04-14 14:12:26 +0200

    it works

--- src/memory.js
+++ src/memory.js
@@ -1,9 +1,11 @@
 /* Elements */
 const cards = document.querySelectorAll('.card')
+const scoreSpan = document.querySelector('#scoreSpan')
 
 /* Variables */
 const animals = ['cat', 'fish', 'lion', 'pig', 'duck', 'cow']
 const cardsAnimals = {
+    0: null,
     1: null,
     2: null,
     3: null,
@@ -15,18 +17,20 @@ const cardsAnimals = {
     9: null,
     10: null,
     11: null,
-    12: null,
 }
+
+let score = 0
+let first = null
+let second = null
+let cardSelected = 0
 
 /* Functions */
 let placeCards = () => {
     let allCards = animals.concat(animals)
-    console.log(allCards)
     Object.keys(cardsAnimals).forEach( (key) => {
         index = Math.floor(Math.random() * allCards.length)
         choice = allCards[index]
         allCards.splice(index, 1)
-        console.log(index, choice, allCards)
 
         cardsAnimals[key] = choice
     });
@@ -38,10 +42,32 @@ placeCards()
 
 cards.forEach( (card, index) => {
     card.addEventListener('click', () => {
-        card.style.backgroundImage = `url(./../img.html/${cardsAnimals[index + 1]}.png`
+        card.style.backgroundImage = `url(./../img.html/${cardsAnimals[index]}.png`
+        if (cardSelected % 2 === 0) first = index
+        else {
+            second = index
+            checkMove(first, second)
+        }
+        cardSelected++
     })
 })
 
-let hideCard = () => {
+let hideCard = (list) => {
+    list.forEach( (card) => {
+        cards[card].style.backgroundImage = 'url(./../img.html/questionMark.png'
+    })
+}
 
+let checkMove = (first, second) => {
+    if (cardsAnimals[first] === cardsAnimals[second]) {
+        score++
+        updateScore()
+    }
+    else {
+        setTimeout(hideCard, 800, [first, second])
+    }
+}
+
+let updateScore = () => {
+    scoreSpan.innerHTML = score
 }
